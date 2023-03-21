64894884b9ca433ef967008e7681c05712d63df3
mucklord <mucklord7@gmail.com>
2023-03-21 10:33:21 +0100

    3 af

---
+++ chalange3.js
@@ -0,0 +1,11 @@
+ const scoreDolfins =(96+108+89) /3;
+ const scoreKoalas = (88+91+110)/3;
+ console.log(scoreDolfins,scoreKoalas);
+ if(scoreDolfins>scoreKoalas){
+    console.log ("Dolfins win")
+ }
+ else if(scoreKoalas>scoreDolfins){
+    console.log("Koalas win")
+ }else if(scoreDolfins===scoreKoalas){
+    console.log("they both win")
+ }
